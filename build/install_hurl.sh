47710fc44fc4ac0809bd08e4fe0b627e8d2596bf
Ivan Shalganov <aco.best@gmail.com>
2023-04-12 09:26:34 +0200

    fix gh actions

--- build/install_hurl.sh
+++ build/install_hurl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-INSTALL_DIR=/tmp
-curl -silent --location https://github.com/Orange-OpenSource/hurl/releases/download/2.0.1/hurl-2.0.1-x86_64-linux.tar.gz | tar xvz -C $INSTALL_DIR
-mv $INSTALL_DIR/hurl-2.0.1 $INSTALL_DIR/hurl
+
+set -exo pipefail
+
+curl -silent --location https://github.com/Orange-OpenSource/hurl/releases/download/2.0.1/hurl-2.0.1-x86_64-linux.tar.gz | tar xvz -C /tmp
+mv /tmp/hurl-2.0.1 /tmp/hurl
