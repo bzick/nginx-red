a5e5005ef00681efb2f6587c29c8d5bed7b6f3c5
Ivan Shalganov <aco.best@gmail.com>
2023-04-12 09:36:05 +0200

    fix hg actions

--- build/install_hurl.sh
+++ build/install_hurl.sh
@@ -2,5 +2,5 @@
 
 set -exo pipefail
 
-curl -silent --location https://github.com/Orange-OpenSource/hurl/releases/download/2.0.1/hurl-2.0.1-x86_64-linux.tar.gz | tar xvz -C /tmp
+wget -q -O - https://github.com/Orange-OpenSource/hurl/releases/download/2.0.1/hurl-2.0.1-x86_64-linux.tar.gz | tar xvz -C /tmp
 mv /tmp/hurl-2.0.1 /tmp/hurl
